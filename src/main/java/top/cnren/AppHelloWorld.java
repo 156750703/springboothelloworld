0c5dee1a22b0309dc18e8189b25c0a1846d65a69
yuanweijun <wjyuan@tplhk.cntaiping.com>
2020-06-06 13:59:53 +0800

    helloworld测试

---
+++ src/main/java/top/cnren/AppHelloWorld.java
@@ -0,0 +1,11 @@
+package top.cnren;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppHelloWorld {
+	public static void main(String[] args) {
+		SpringApplication.run(AppHelloWorld.class,args);
+	}
+}
